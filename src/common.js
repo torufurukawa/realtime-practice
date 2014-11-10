d739854a0db45490e04d9b3ba2548c5e34f15adb
Toru Furukawa <furukawa@bascule.co.jp>
2014-11-10 18:02:32 +0900

    Add publish and subscribe

--- src/common.js
+++ src/common.js
@@ -2,14 +2,26 @@ var client;
 
 function connect(clientId, onSuccess, onFailure){
     client = new Paho.MQTT.Client(config.sango_server, clientId);
-
-    // connectします
     client.connect({userName: config.sango_user, password: config.sango_pass,
                     onSuccess:onSuccess, onFailure: onFailure});
 }
 
-connect(
-  'operator',
-  function(data){console.log('success'); console.log(data)},
-  function(data){console.log('failure'); console.log(data)}
-);
+function publish(data){
+    var topic = config.sango_user + "/data";
+    message = new Paho.MQTT.Message(data);
+    message.destinationName = topic;
+    client.send(message);
+}
+
+// メッセージが到着したら呼び出されるコールバック関数
+ function onMessageArrived(message) {
+     console.log("onMessageArrived:"+message.payloadString);
+ }
+
+function subscribe(callback){
+    // コールバック関数を登録します
+    client.onMessageArrived = callback;
+
+    var topic = config.sango_user + "/data";
+    client.subscribe(topic);
+}
