a97a1ba791c0ac11617b7d64eb1d396b4eb1517f
Toru Furukawa <torufurukawa@gmail.com>
2014-11-16 10:08:11 +0900

    Refactor

--- src/common.js
+++ src/common.js
@@ -25,3 +25,28 @@ function subscribe(callback){
     var topic = config.sango_user + "/data";
     client.subscribe(topic);
 }
+
+
+function Sango(server, user, pass) {
+    this.server = server;
+    this.user = user;
+    this.pass = pass;
+
+    this.connect = function(clientId, onSuccess, onError) {
+        this.client = new Paho.MQTT.Client(this.server, clientId);
+        this.client.connect({userName: this.user, password: this.pass,
+                             onSuccess:onSuccess, onFailure: onError});
+
+    };
+
+    this.publish = function(topic, data) {
+        var message = new Paho.MQTT.Message(data);
+        message.destinationName = topic;
+        this.client.send(message);
+    };
+
+    this.subscribe = function(topic, callback) {
+        this.client.onMessageArrived = function(message) {callback(message.payloadString);};
+        this.client.subscribe(topic);
+    };
+};
