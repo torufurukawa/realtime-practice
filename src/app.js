986468413a97d81af6d553f0fe859f6bc69f6661
Toru Furukawa <furukawa@bascule.co.jp>
2014-11-17 13:46:51 +0900

    Migrate src/operator.html to src/partials/operator.html

--- src/app.js
+++ src/app.js
@@ -10,7 +10,32 @@ app.config(['$routeProvider',
         templateUrl: 'partials/top.html'
         // ,controller: 'PhoneListCtrl'
       }).
+      when('/operator', {
+        templateUrl: 'partials/operator.html',
+        controller: 'OperatorCtrl'
+      }).
       otherwise({
         redirectTo: '/'
       });
   }]);
+
+
+// Controllers
+
+app.controller('OperatorCtrl', ['$scope', function($scope) {
+  $scope.init = function() {
+    $scope.topic = config.sango_user + "/data";
+    $scope.sango = new Sango(config.sango_server, config.sango_user, config.sango_pass);
+    $scope.sango.connect(
+      'operator',
+      function(data){console.log('connected');},
+      function(data){console.log('connecton failed'); console.log(data);});
+  };
+
+  $scope.updateStatus = function(status) {
+    $scope.sango.publish($scope.topic, status, true);
+    $scope.status = status;
+  };
+
+  $scope.init();
+}]);
